7ae6fa10a25076f9ab8fedfccf8e3849d3615447
edil3ra <edil3ra@gmail.com>
2023-01-31 13:44:25 +0100

    feat: add parent to environment for block statement

--- environment.py
+++ environment.py
@@ -4,19 +4,27 @@ from runtimeerror import RuntimeError
 
 class Environment:
     values: Dict
+    enclosing: Environment
+
     def __init__(self):
+        self.enclosing = None
         self.values = {}
 
     def define(self, name: str, value: Any):
         self.values[name] = value
     
-    def get(self, name: Token):
+    def get(self, name: Token) -> Any:
         if name.lexeme in self.values.keys():
             return self.values[name.lexeme]
+        if self.enclosing is not None:
+            return self.enclosing.get(name)
         raise RuntimeError(name, f'Undefined variable "{name.lexeme}".')
 
     def assign(self, name: Token, value: Any):
         if name.lexeme in self.values.keys():
             self.values[name.lexeme] = value
+            return
+        if self.enclosing is not None:
+            self.enclosing.assign(name, value)
             return
         raise RuntimeError(name, f'Undefined variable "{name.lexeme}".')
