56b67310cf6db59b2e24d4dfec28400ce25b26bd
edil3ra <edil3ra@gmail.com>
2023-02-01 22:14:08 +0100

    prettier: main

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from lox import Lox
 import expr
-from tokens import Token
-from token_type import TokenType
 from ast_printer import AstPrinter
+from lox import Lox
+from token_type import TokenType
+from tokens import Token
+
 
 def test_expr():
     expression = expr.Binary(
         expr.Unary(
-            Token(TokenType.MINUS, '-', None, 1),
+            Token(TokenType.MINUS, "-", None, 1),
             expr.Literal(123),
         ),
-        Token(TokenType.STAR, '*', None, 1),
-        expr.Grouping(
-            expr.Literal(45.67)
-        )
+        Token(TokenType.STAR, "*", None, 1),
+        expr.Grouping(expr.Literal(45.67)),
     )
     return AstPrinter().print(expression)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     lox = Lox()
     lox.main()
-    
